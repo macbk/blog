8984971d27c568fd8cb8de9bfd9af10dea6e91e6
macb <macb88@gmail.com>
2017-01-09 14:28:13 +0000

    Add User to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ActiveRecord::Base
+    belongs_to :user
     has_many :comments, dependent: :destroy
     validates :title, presence: true, length: { maximum: 50 }
     validates :body, presence: true, length: { maximum: 300 }
